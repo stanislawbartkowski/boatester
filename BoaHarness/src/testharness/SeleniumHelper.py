669b430ffc73ff062005c6824b237b60db4da302
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com@7b462660-130a-11df-8b85-374ad9ff7b16>
2010-09-27 20:41:00 +0000

    Selenium helper

---
+++ BoaHarness/src/testharness/SeleniumHelper.py
@@ -0,0 +1,402 @@
+#
+# Copyright [2010] the stanislaw.bartkowski@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""  Helper for running Selenium test
+"""
+
+__authors__ = [
+    '"Stanislaw Bartkowski" <stanislawbartkowski@gmail.com>',
+]
+
+import logging
+import TestCaseHelper
+import TestBoa
+import os
+import os.path
+import time
+
+def _readC(fname):
+   """ Produces ConfigP object for reading selenium files
+    
+    Args:
+      fname : configuration file name (full path)
+      
+    Returns:
+      ConfigP object created
+      
+    Raise:
+      TestException if file does not exist
+    """
+   if fname == None : return None
+   TestBoa.exist(fname)
+   logging.debug("Read selenium config file from : " + fname)
+   co = ConfigP(fname)
+   return co
+
+class ConfigP :
+
+    """ Helper class for maintaining selenium configurationf iles
+    """
+
+    def __init__(self,fname):
+        """ Constructor for ConfigP file
+
+        Args:
+          fname : configuration file name, full path
+        """
+        f = open(fname)
+        self.lines = f.readlines()
+        f.close()
+
+    """ Returns sequence of selenium action to execute
+
+    Args:
+      secname : section name in configuraiton file to read
+
+    Returns:
+      Sequence of action elem (actionname, (list of params))
+
+    """
+    def getSec(self,secname):
+        secname = '[' + secname + ']'
+        res = None
+        for l in self.lines :
+            ll = l.strip()
+            if ll == "" : continue
+            if ll[0] == ';' : continue
+            if ll[0] == '-' : continue
+            if res == None :
+                if ll == secname :
+                    res = []
+                continue
+            if ll[0] == '[' : break
+            a = ll.split(':',1)
+            if len(a) > 1 :
+               res.append((a[0].strip(),a[1].strip()))
+            else :
+               res.append((a[0].strip(),None))
+        return res
+
+    """ Getter for option name from [alias] section
+
+    Args:
+      key : key of option to return
+
+    Returns:
+      option value or None if option does not exist
+
+    """
+    def getOption(self,key):
+        res = self.getSec('alias')
+        if res == None : return None
+        for a in res :
+            ke = a[0]
+            va = a[1]
+            if ke == key : return va
+        return None
+
+
+class ConfigFileHelper :
+    """ Helper for maintaing selenium configuration file
+
+    """
+
+    def __init__(self,mainfile,testfile):
+        """ Constructor
+
+        Args:
+          mainfile : file path name of global selenium file (secondary)
+          testfile : file  path name of test selenium file (primary)
+
+        """
+
+        self.mainco = _readC(mainfile)
+        self.testco = _readC(testfile)
+
+        
+    def __getAlias(self,key):
+        """ Getter for alias value. Firsty primary file is searched,
+            than secondary file
+
+        Args:
+          key : alias key
+
+        Returns:
+          alias value
+
+        Raise:
+           TestException if alias does not exist
+
+        """
+        if self.testco != None :
+           al = self.testco.getOption(key)
+           if al != None : return al
+        if self.mainco != None :
+           al = self.mainco.getOption(key)
+           if al != None : return al
+        raise TestCaseHelper.TestException(key + " no alias with that name")
+
+    def getSequence(self,name):
+        """ Returns sequence of selenium actions
+
+        Args:
+          name : test name (section name)
+
+        Returns:
+          Sequence of action. Action if firstly search in primary file
+          and (if not found) in secondary file
+
+        Raise:
+           TestException if name does not exist
+        """
+        keys = None
+        if self.testco != None :
+            keys = self.testco.getSec(name)
+        if keys == None :
+            if self.mainco != None :
+               keys = self.mainco.getSec(name)
+        if keys == None :
+           raise TestCaseHelper.TestException(name + " no such section")
+        res = []
+        for k in keys :
+            ke = k[0]
+            va = k[1]
+            pa = []
+            if va != None:
+                pa = va.split('|')
+                for i in range(0,len(pa)) :
+                  ss = pa[i]
+                  ch = ss[0]
+                  if ch == '#' :
+                    re = ss[1:]
+                    pa[i] = self.__getAlias(re)
+            res.append((ke,pa))
+        return res
+
+class seleniumTypeContext :
+    """  Base class for extending by action handler
+    """
+
+    def __init__(self) :
+        """ Constructor - empty for the time being
+        """
+        pass
+
+    def setParam(self,cparam,se,tcase,param,teparam) :
+        """ Set context param. Called by test runner
+        
+         Args:
+           cparam :  ConfigFileHelper refrence
+           se : Selenium class
+           tcase : unittest.TestCase class containing this test
+           param : TestParam container
+           tepar : OneTestParam container
+        
+        """
+        if cparam != None :
+            logging.debug(self.key + " " + str(cparam))
+        else :
+            logging.debug(self.key)
+
+        if cparam == None or len(cparam) < self.nof :
+            raise TestCaseHelper.TestException(self.key + " requires " + str(self.nof) + " parameters! Actual number : " +  str(cparam))
+           
+        self.se = se
+        self.cparam = cparam
+        self.tcase = tcase
+        self.param = param
+        self.teparam = teparam
+        
+    def getParam(self):
+        """ Getter for params
+        
+        Returns:
+          Look setParam
+        """
+        return (self.ce, self.cparam, self.tcase, self.param, self.teparam)
+
+    def setInfo(self,key,nof) :
+        """ Set additonal information for action handler
+        
+        Args:
+          key : action name
+          nof : number of parameters
+        """
+        self.key = key
+        self.nof = nof
+
+def waitForElement(tcase,sel,ele,sec=5) :
+    """ Waits for element on the screen
+    
+    Args:
+       tcase : Unittest class
+       sel : Selenium type
+       ele : element to wait for
+       sec : maximum number of second to wait for
+       
+     Raise:
+       fails test case if time expires
+       
+    """
+    for i in range(1,5) :
+        if sel.is_element_present(ele) : return
+        time.sleep(1)
+    tcase.fail(ele + " cannot find that element !")
+        
+
+class seleniumType(seleniumTypeContext) :
+    """ Action class for 'type' action 
+    First parameter - element selector
+    Second parameter - string to be typed in
+    """
+    def do(self):
+        self.se.type(self.cparam[0], self.cparam[1])
+        
+class seleniumClick(seleniumTypeContext) :
+    """ Action class for 'click' action
+    First parameter: element selector to be clicked
+    """
+
+    def do(self):
+        id = self.cparam[0]
+        self.se.click(id)
+        
+class seleniumWait(seleniumTypeContext) :
+    """ Action class for 'sleep' action
+    First parameter: number of seconds to slepp
+    """
+
+    def do(self):
+        nosec = int(self.cparam[0])
+        time.sleep(nosec)
+        
+class seleniumDebug(seleniumTypeContext) :
+    """ Empty class only for debug purpose
+    Allows to set breakpoint
+    """
+
+    def do(self):
+        pass
+        
+class seleniumWaitFor(seleniumTypeContext) :
+    """ Action class for 'waitFor' action
+    First parameter: element selector to be waited
+    """
+
+    def do(self):
+        waitForElement(self.tcase,self.se,self.cparam[0])
+
+class seleniumIsPresent(seleniumTypeContext) :
+    """ Action class for 'isPresent' action
+    First parameter: element selector to be searched
+    """
+
+    def do(self):
+        ele = self.cparam[0]
+        ispresent= self.se.is_element_present(ele)
+        self.tcase.assertTrue(ispresent, ele + " not present")
+
+class seleniumVerEqual(seleniumTypeContext) :
+    """ Action class for 'verEqual' verification action
+    First parameter: element selector
+    Second parameter: string to be verified 
+    """
+
+    def do(self):
+        id = self.cparam[0]
+        vermess = self.cparam[1]
+        mess = self.se.get_value(id)
+        self.tcase.assertEqual(vermess, mess)
+
+class SeleniumHelper :
+    """ Helper supporting selenium action
+    """
+
+    def __init__(self,se):
+        """ Constructor
+        
+        Args:
+          se - Selenium class
+        """
+        self.se = se
+        self.a = {}
+        self.registerAction('type',seleniumType(),2)
+        self.registerAction('click',seleniumClick())
+        self.registerAction('sleep',seleniumWait())
+        self.registerAction('waitFor',seleniumWaitFor())
+        self.registerAction('debug',seleniumDebug(),0)
+        self.registerAction('verEqual',seleniumVerEqual(),2)
+        self.registerAction('isPresent',seleniumIsPresent(),1)
+
+    def registerAction(self,key,o,nofParam = 1):
+        """ Register custom action handler
+        
+        Args:
+          key : action name
+          o : action handler (extends SeleniumTypeContext)
+          nofParam : number of parameters expected
+        """
+        logging.debug('Register action ' + key + ' num of params:' + str(nofParam))
+        o.setInfo(key,nofParam)
+        self.a[key] = o
+        
+
+    def setParam(self,tcase,param,teparam):
+        """ Sets param
+        
+        Args:
+          tcase : unittest.TestCase
+          param : TestParam container
+          tepar : OneTestParam container
+        """
+        self.tcase = tcase
+        self.param = param
+        self.teparam = teparam
+        
+    def getParam(self):    
+        """ Getter
+        Returns:
+          Look setParam
+        """
+        return (self.tcase, self.param, self.teparam)
+
+    def runTest(self,name):
+        """ Runs test 'name'
+        
+        Args:
+          name : test name to be run
+          
+        Raise:
+           Fails test in case of error
+        """
+        mainfile = None
+        gName = self.param.getPar('selfile')
+        if gName != None :
+           resDir = self.param.getGlobResDir()
+           mainfile = os.path.join(resDir, gName)
+        testfile = None
+        tefile = self.teparam.getPar('selfile')
+        if tefile != None :
+            id = self.teparam.getTestId()
+            teDir = self.param.getTestDir(id)
+            testfile = os.path.join(teDir, tefile)
+        co = ConfigFileHelper(mainfile,testfile)
+        seq = co.getSequence(name)
+        for step in seq :
+            key = step[0]
+            param = step[1]
+            action = self.a[key]
+            action.setParam(param,self.se,self.tcase,self.param,self.teparam)
+            action.do()
